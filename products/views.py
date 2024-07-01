b6f3a7a8313ff93d56657da3f2096bf9741a49ca
sheakin <sheakin92@gmail.com>
2024-07-02 01:26:25 +0530

    frontend changes

---
+++ products/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def list_products(request):
+    """
+    returns product list page
+
+    Args:
+        request (_type_): _description_
+    """
+    return render (request,'products.html')
+
+def detail_product(request):
+    return render(request,'product_detail.html')
